7c309a3dbd32bae4da065be82329bc7995a04506
omahmoud9 <92744697+omahmoud9@users.noreply.github.com>
2022-01-15 23:10:56 -0500

    Create treasre_island.py

---
+++ day3/treasre_island.py
@@ -0,0 +1,54 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+choice = input('There are 2 paths, left and right? Type "Left" or "Right". \n')
+if choice.lower() == "right":
+  print("You take the right path....")
+  print("Youve fallen into a a forgot booby trap.")
+  print("Game Over")
+
+print("At the end of the left path youve found a lake with a island in sight.") 
+choice = input('Do you swim or wait for a boat? Type "Swim or "Wait".\n')
+
+if choice.lower() == "swim":
+  print("You begin to swim....")
+  print("You rember you had lunch 10 min ago...")
+  print("You begin to cramp up")
+  print("You drown....")
+  print("Game over")
+
+choice = input('You come across 3 doors, Red, Blue, and Yellow, Type "Red", "Blue", or Yellow". \n')
+
+if choice.lower() != "red":
+  print("You've chosen the wrong door.")
+  print("Game Over.")
+elif choice.lower() == "red":
+  print("You've chosen the Red Door")
+  print("...")
+  print("Congratulation! You've Won!")
